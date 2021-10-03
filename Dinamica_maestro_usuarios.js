2a5fadc26608738607ea2bc6b39432d82c298711
Diego Garcia <diego.garcia093@gmail.com>
2021-10-03 21:08:02 +0200

    Subir interfaz de maestro de usuario
    Subir primera versión de la interfaz de maestro de usuarios
    
    Diego García, 03 de Octubre de 2021, 14:07

---
+++ Dinamica_maestro_usuarios.js
@@ -0,0 +1,8 @@
+function guardarCambio () {
+alert("El usuario ha sido actualizado con exito");
+}
+
+function editarUsuario (){
+    document.getElementById("rolUsuario").disabled = false;
+    document.getElementById("estadoUsuario").disabled = false;
+}
