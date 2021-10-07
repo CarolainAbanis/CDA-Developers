61e6c132cb53a7a2ea820cfee97b5b234c0b2d20
Carteaga23 <90408360+Carteaga23@users.noreply.github.com>
2021-10-07 20:30:35 +0100

    Interfaz módulo registro de ventas- 07/10/21

---
+++ CDA-Developers/Botones.js
@@ -0,0 +1,6 @@
+function guardarCambio () {
+    alert("Su venta ha sido guardada con exito");
+    }
+    function actualizarCambio () {
+        alert("Su venta ha sido actualizada con exito");
+    }
